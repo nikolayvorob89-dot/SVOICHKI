197b76b31d08537b3f459320b0b698f6652cf037
Nikolay <Bebek@MacBook-Air-Bugaeva.local>
2025-12-05 02:11:28 +0500

    Fix duplicate messages in chat

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
 import eventlet
 import os
 
-eventlet.monkey_patch()  # обязательно для WebSocket
+eventlet.monkey_patch()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -15,8 +15,9 @@ def index():
 
 @socketio.on('message')
 def handle_message(msg):
-    send(msg, broadcast=True)
+    # отправляем только другим клиентам
+    send(msg, broadcast=True, include_self=False)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # берем порт от Render
+    port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host='0.0.0.0', port=port)
